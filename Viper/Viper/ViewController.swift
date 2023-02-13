6457c7fbb9df4a3ad3bfd3fe17b2abbb7ede2a5c
IamTort <61164603+IamTort@users.noreply.github.com>
2023-02-13 16:56:33 +0300

    init

---
+++ Viper/Viper/ViewController.swift
@@ -0,0 +1,11 @@
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// Контроллер
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
